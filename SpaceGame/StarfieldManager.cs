c6ab384fe72a4de42b489ae78305483309f18626
dandonnan <dandonnan@users.noreply.github.com>
2018-02-23 13:00:06 +0000

    Lunchtime Day 5 pt2 - Constant star generation

--- SpaceGame/StarfieldManager.cs
+++ SpaceGame/StarfieldManager.cs
@@ -12,20 +12,37 @@ namespace SpaceGame
     public class StarfieldManager
     {
         List<StarfieldObject> objects;
+        Texture2D starfield;
+
+        int minTime;
+        int maxTime;
+        int timeUntilNext;
+
+        bool planetActive = false;
 
         public StarfieldManager(ContentManager cm)
         {
             objects = new List<StarfieldObject>();
+            starfield = cm.Load<Texture2D>("starfield");
 
-            objects.Add(new StarfieldObject(cm.Load<Texture2D>("starfield"), new Vector2(1280, 5), 3, 1, StarfieldObject.Predefined.SmallStar));
-            objects.Add(new StarfieldObject(cm.Load<Texture2D>("starfield"), new Vector2(1300, 360), 3, 3, StarfieldObject.Predefined.SmallStar));
-            objects.Add(new StarfieldObject(cm.Load<Texture2D>("starfield"), new Vector2(1450, 590), 3, 1, StarfieldObject.Predefined.SmallStar));
-            objects.Add(new StarfieldObject(cm.Load<Texture2D>("starfield"), new Vector2(1350, 682), 0.1f, 1, StarfieldObject.Predefined.Planet));
-        
+            minTime = 1;
+            maxTime = 5;
+
+            objects.Add(new StarfieldObject(starfield, new Vector2(1350, 682), 0.1f, 1, StarfieldObject.Predefined.Planet));
         }
 
         public void Update()
         {
+            timeUntilNext--;
+
+            if (timeUntilNext <= 0)
+            {
+                Random rand = new Random();
+                timeUntilNext = rand.Next(minTime, maxTime + 1);
+                int y = rand.Next(0, 682);
+                objects.Add(new StarfieldObject(starfield, new Vector2(1280, y), 3, 1, StarfieldObject.Predefined.SmallStar));
+            }
+
             if (objects.Count > 0)
             {
                 for (int i = objects.Count - 1; i >= 0; i--)
